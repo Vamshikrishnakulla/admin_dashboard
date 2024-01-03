49a3d9239e9ed1d1b294de9799a0ae2d0e5c612e
vamshi <vamshi.kk67@gmail.com>
2024-01-03 12:25:36 +0530

    update charts

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,35 @@
 var options = {
   series: [
     {
-      name: "Net Profit",
-      data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-    },
-    {
-      name: "Revenue",
-      data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-    },
-    {
-      name: "Free Cash Flow",
-      data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+      name: "Desktops",
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
     },
   ],
   chart: {
-    type: "bar",
     height: 350,
-  },
-  plotOptions: {
-    bar: {
-      horizontal: false,
-      columnWidth: "55%",
-      endingShape: "rounded",
+    type: "line",
+    zoom: {
+      enabled: false,
     },
   },
   dataLabels: {
     enabled: false,
   },
   stroke: {
-    show: true,
-    width: 2,
-    colors: ["transparent"],
+    curve: "straight",
+  },
+  title: {
+    text: "Product Trends by Month",
+    align: "left",
+  },
+  grid: {
+    row: {
+      colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+      opacity: 0.5,
+    },
   },
   xaxis: {
-    categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
-  },
-  yaxis: {
-    title: {
-      text: "$ (thousands)",
-    },
-  },
-  fill: {
-    opacity: 1,
-  },
-  tooltip: {
-    y: {
-      formatter: function (val) {
-        return "$ " + val + " thousands";
-      },
-    },
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
   },
 };
 
